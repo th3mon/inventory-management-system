fbd6b445820cbcea0315159395aa4de1e25f67b8
th3mon <p.szelenberger@gmail.com>
2026-01-02 15:33:17 +0100

    Wire up GetProducts Query

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,10 @@ import errorHandler from "@/common/middleware/errorHandler";
 import rateLimiter from "@/common/middleware/rateLimiter";
 import requestLogger from "@/common/middleware/requestLogger";
 import { env } from "@/common/utils/envConfig";
+import { productRouter } from "./api/product/product.router";
+import { QueryBus } from "./common/cqrs";
+import { GetProductsHandler } from "./api/product/get-products";
+import { ProductReadRepository } from "./api/product";
 
 const logger = pino({ name: "server start" });
 const app: Express = express();
@@ -29,6 +33,11 @@ app.use(requestLogger);
 // Routes
 app.use("/health-check", healthCheckRouter);
 app.use("/users", userRouter);
+
+const productReadRepository = new ProductReadRepository();
+const queryBus = new QueryBus();
+queryBus.register(new GetProductsHandler(productReadRepository));
+app.use("/products", productRouter(queryBus));
 
 // Swagger UI
 app.use(openAPIRouter);
