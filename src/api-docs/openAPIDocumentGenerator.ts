937be3e38eb80b30b8cf68c93b94980247b8ad0b
th3mon <p.szelenberger@gmail.com>
2026-01-02 08:20:15 +0100

    Use spaces as indentStyle

--- src/api-docs/openAPIDocumentGenerator.ts
+++ src/api-docs/openAPIDocumentGenerator.ts
@@ -6,18 +6,18 @@ import { userRegistry } from "@/api/user/userRouter";
 export type OpenAPIDocument = ReturnType<OpenApiGeneratorV3["generateDocument"]>;
 
 export function generateOpenAPIDocument(): OpenAPIDocument {
-	const registry = new OpenAPIRegistry([healthCheckRegistry, userRegistry]);
-	const generator = new OpenApiGeneratorV3(registry.definitions);
+  const registry = new OpenAPIRegistry([healthCheckRegistry, userRegistry]);
+  const generator = new OpenApiGeneratorV3(registry.definitions);
 
-	return generator.generateDocument({
-		openapi: "3.0.0",
-		info: {
-			version: "1.0.0",
-			title: "Swagger API",
-		},
-		externalDocs: {
-			description: "View the raw OpenAPI Specification in JSON format",
-			url: "/swagger.json",
-		},
-	});
+  return generator.generateDocument({
+    openapi: "3.0.0",
+    info: {
+      version: "1.0.0",
+      title: "Swagger API",
+    },
+    externalDocs: {
+      description: "View the raw OpenAPI Specification in JSON format",
+      url: "/swagger.json",
+    },
+  });
 }
